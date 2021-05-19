aac9acb2fa73ce142bfb1776741e001581f564b0
Evgeny Grebenshchikov <eugeny.grebenshchikov@db.com>
2021-05-19 16:43:10 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "network" {
   source  = "app.terraform.io/evgenygrebenshchikov-training/network/google"
-  version = "2.5.0"
+  version = "~> 2.5"
 
   project_id   = var.project
     network_name = "gaurav-network"
