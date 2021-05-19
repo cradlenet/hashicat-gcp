ae96182d4d86ebc544c9eb2172bee5d614e00e2f
Evgeny Grebenshchikov <eugeny.grebenshchikov@db.com>
2021-05-19 16:59:52 +0000

    Added vpc module

---
+++ vpc.tf
@@ -0,0 +1,17 @@
+module "network" {
+  source  = "app.terraform.io/evgenygrebenshchikov-training/network/google"
+  version = "~> 2.5"
+
+  project_id   = var.project
+    network_name = "gaurav-network"
+    routing_mode = "GLOBAL"
+
+    subnets = [
+        {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+        }
+    ]
+
+}
